23749b0d0b145aa65072a170ec7a7c3edd55c31a
softwaredonald <xfdonald@gmail.com>
2018-08-17 11:53:16 +1000

    强制文件命名规则

--- wdf_no_ui.py
+++ wdf_no_ui.py
@@ -4,10 +4,7 @@ import difflib
 import time
 import os
 
-## 文件名称和文件格式的规范化的代码因为涉及到有版权的文件，暂不提供，但是后期可能会放在无版权申诉条件的加密网盘中
 
-
-##下面这个UI界面，暂时不考虑
 def windows(self):
     window = Tk()
     window.title("PTE的write from dictionary练习")
@@ -17,11 +14,11 @@ def windows(self):
     self.sentence = StringVar()
     entrysentence = Entry(frame1,textvarible = self.sentence)
 
-def readfromfile():
+def readfromfile(name):
     memlist = {}
     i = 1
-	## 修改文件路径
-    for line in open("*******"):
+    	
+    for line in open(name):
         memlist[i] = line
         i=i+1
     return memlist
@@ -31,14 +28,14 @@ def dictout(memlist,i):
     sentence = sentence[:-1]
     return sentence
 
-def writetofile(sen,i):
-	fo = open("pra\\1872.txt","a")
+def writetofile(sen,i,filename):
+	fo = open(filename,"a")
 	fo.write(str(i)+".      "+sen+"\n")
 	fo.close
 
 	
-def writetofilecorrection(dictsen,i):
-	fo = open("pra\\1872.txt","a")
+def writetofilecorrection(dictsen,i,filename):
+	fo = open(filename,"a")
 	fo.write(str(i)+".corr  "+dictsen+"\n")
 	fo.close
 
@@ -50,29 +47,46 @@ def matching(sen, dictsen):
     d = difflib.Differ()
     diff = d.compare(sen.splitlines(),dictsen.splitlines())
     return diff
-	
-## 文件名需要规范	
-def musicplay(i):
-    musicname = "*******************"
+		
+def musicplay(i,floder):
+    musicname = floder+str(i)+".m4a"
     os.system(musicname)
     return 6
 
 print("PTE WDF练习运行")
-dictinfo = readfromfile()
+option = input("请选择WDF的题库，1:2018年7月题库；2:2018年3月题库；3;2017年10月题库；4:2017年5月题库")
+if option =="1":
+    nameofaudiotext = "audiotext\\187.txt"
+    nameofaudiofolder = "187\\"
+    nameofwritingfile = "pra\\187.txt"
+elif option =="2":
+    nameofaudiotext = "audiotext\\183.txt"
+    nameofaudiofolder = "183\\"
+    nameofwritingfile = "pra\\183.txt"
+elif option =="3":
+    nameofaudiotext = "audiotext\\1710.txt"
+    nameofaudiofolder = "1710\\"
+    nameofwritingfile = "pra\\1710.txt"
+elif option =="4":
+    nameofaudiotext = "audiotext\\175.txt"
+    nameofaudiofolder = "175\\"
+    nameofwritingfile = "pra\\175.txt"
+
+dictinfo = readfromfile(nameofaudiotext)
 dictlen =  len(dictinfo)
 i=1
 for i in range(1,dictlen+1):
-    time.sleep(musicplay(i))
+    time.sleep(musicplay(i,nameofaudiofolder))
     sen = readfromtype(i)
-    writetofile(sen,i)
+    writetofile(sen,i,nameofwritingfile)
     
     dictsen = dictout(dictinfo,i)
-    writetofilecorrection(dictsen,i)
+    writetofilecorrection(dictsen,i,nameofwritingfile)
     diff = matching(sen,dictsen)
     print("this is the matching result")
     print(sen)
     print("\n".join(list(diff)))
     time.sleep(2)
     print("let move to next question")
-    
+print("This type Question has finished.")
 
